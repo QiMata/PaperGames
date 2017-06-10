c9d2475beafed6e2ee7280dca87266a2ce0ac548
MERCURY\Meche <mcorry@fusethru.com>
2017-06-10 14:49:58 -0400

    Added Menu for game

---
+++ Sample Hololens/Assets/MainScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour {
+    private Button _mainTextButton;
+	// Use this for initialization
+	void Start () {
+        _mainTextButton = GameObject.Find("TestMain").GetComponent<Button>();
+        _mainTextButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(2);
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
