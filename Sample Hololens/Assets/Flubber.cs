a91ab992258ff0ac3ec63382a9aff65f93dedf15
Jared Rhodes <jrhodes@corefount.com>
2017-06-10 16:28:30 -0400

    added in the ability to respond to voice commands

---
+++ Sample Hololens/Assets/Flubber.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flubber : MonoBehaviour
+{
+    private Rigidbody _ridgidBody;
+
+    void Start()
+    {
+        _ridgidBody = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        _ridgidBody.AddForce(new Vector3(collision.impulse.x * 50, collision.impulse.y * 50, collision.impulse.z * 50));
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+}
