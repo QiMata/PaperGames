4489b9eda5d4bcd6f23960c7e5ac67a2a0983e67
Jared Rhodes <jrhodes@corefount.com>
2017-06-10 14:35:29 -0400

    Added in new meshes and colliders

--- Sample Hololens/Assets/GameManager.cs
+++ Sample Hololens/Assets/GameManager.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    private TextMesh _goal;
     private LaunchableObject _football;
+    private GoalPostScript _goalPost;
     private bool _lanched;
 	// Use this for initialization
 	void Start () {
         _football = GameObject.Find("PaperFootball").GetComponent<LaunchableObject>();
-        
+        _goal = GameObject.Find("Goal").GetComponent<TextMesh>();
+        _goalPost = GameObject.Find("GoalPost").GetComponentInChildren<GoalPostScript>();
+        _goalPost.Reset();
     }
 	
 	// Update is called once per frame
@@ -16,7 +20,12 @@ public class GameManager : MonoBehaviour {
     {
         if (!_lanched)
         {
-            _football.Launch(0, 0, -15);
+            _football.Launch(0, 0, -600);
+            _lanched = true;
+        }
+        if (_goalPost.WentThroughUpright)
+        {
+            _goal.text = "Goal!!!";
         }
 	}
 }
--- Sample Hololens/Assets/GoalPostScript.cs
+++ Sample Hololens/Assets/GoalPostScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GoalPostScript : MonoBehaviour
 {
-    public bool WentThroughUpright { get; set; }
+    public bool WentThroughUpright { get; private set; }
                                        // Use this for initialization
     void OnTriggerEnter(Collider other)
     {
